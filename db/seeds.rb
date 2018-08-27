1d2febf4ca68c7527fa11cded66a6cebbf27f104
Tim Fielder <fiel1139@gmail.com>
2018-08-26 21:48:01 -0600

    makes style changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+artist_1 = Artist.create(name: "Rick James")
+artist_2 = Artist.create(name: "Vanilla Ice")
+song_1 = artist_1.songs.create(title: 'Superfreak', length: 300, play_count: 5000000)
+song_2 = artist_1.songs.create(title: 'Ice Ice Baby', length: 200, play_count: 10000)
+song_3 = artist_1.songs.create(title: 'De colores', length: 200, play_count: 50000)
+song_4 = artist_1.songs.create(title: 'Day-O', length: 200, play_count: 50000)
+song_5 = artist_1.songs.create(title: 'Guantanamera', length: 200, play_count: 50000)
+song_6 = artist_1.songs.create(title: 'La vida loca', length: 200, play_count: 50000)
+song_7 = artist_1.songs.create(title: 'Ai ai', length: 200, play_count: 50000)
+song_8 = artist_1.songs.create(title: 'Peace', length: 200, play_count: 50000)
